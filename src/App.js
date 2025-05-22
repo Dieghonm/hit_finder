7e1089495085620e555417db56e7351a6f8cdacb
Diegho Moraes <dieghomoraes@MacBook-Air-de-Diegho.local>
2025-05-22 15:25:08 -0300

    Altera BrowserRouter para HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Body from './components/Body';
